fe144709afd577806df63d6a585acbbadc2bf350
VicenteAros <64981316+VicenteAros@users.noreply.github.com>
2021-10-28 16:12:32 -0300

    Update main.c

--- main.c
+++ main.c
@@ -22,7 +22,59 @@ int main(int argc, char* argv[]){
 	tablero[5][0] = 11; //forward 5
 	tablero[0][0] = 12; //START
 	
-	mostrarTablero(tablero);
+	for(int y=7; y>=0; y--){
+		printf("--------------------------------------------------------------------------------------------------------\n");
+		for(int x=0; x<8; x++){
+			
+			switch(tablero[x][y]){
+				case 0:
+					printf("|    +50    |");
+					break;
+				case 1:
+					printf("|    -25    |");
+					break;
+				case 2:
+					printf("|    +75    |");
+					break;
+				case 3:
+					printf("|    -50    |");
+					break;
+				case 4:
+					printf("|   Jail    |");
+					break;
+				case 5:
+					printf("|    -75    |");
+					break;
+				case 6:
+					printf("|   Free    |");
+					break;
+				case 7:
+					printf("|   back 2  |");
+					break;
+				case 8:
+					printf("|   back 3  |");
+					break;
+				case 9:
+					printf("|   back 4  |");
+					break;
+				case 10:
+					printf("| forward 3 |");
+					break;
+				case 11:
+					printf("| forward 5 |");
+					break;
+				case 12:
+					printf("|   Start   |");
+					break;
+				default:
+					printf("             ");
+			}
+			if(x == 7){
+				printf("\n");
+			}
+		}
+	}
+	printf("--------------------------------------------------------------------------------------------------------\n");
 	
 	return(0);
 }
