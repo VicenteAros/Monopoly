8be681cd4eedc206c1ec192d5dafd202295da141
VicenteAros <64981316+VicenteAros@users.noreply.github.com>
2021-11-01 20:36:46 -0300

    Update jueguito.h

--- jueguito.h
+++ jueguito.h
@@ -2,5 +2,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+typedef struct {
+  int dinero = 100;
+  int posicion[2] = {0};
+}jugador;
+
 void mostrarTablero(int *tablero);
 int rollD6(int randomNumber);
+void moverJugador(jugador player, int espacios);
+void aplicarCasilla(int *tablero, jugador player);
